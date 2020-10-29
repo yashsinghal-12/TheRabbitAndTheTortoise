1a0f2cf6b717fb785b0804b8125e73d01fc21ecd
yashsinghal-12 <yash.singhal_cs19@gla.ac.in>
2020-10-29 04:33:17 -0700

    add the private fields, constructor, and the getter method in Tortoise.java

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -1,6 +1,17 @@
 package thread.tortoise;
 
 public class Tortoise implements Runnable {
+    private static final int MILESTONES = 5;
+    private final Thread tortoise;
+
+    public Tortoise() {
+        this.tortoise = new Thread(this, "Tortoise");
+    }
+
+    public Thread getRabbit() {
+        return tortoise;
+    }
+
 
     @Override
     public void run() {
