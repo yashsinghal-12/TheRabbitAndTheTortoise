8ed7fcd916b0ee502816d4c5ff4d19e9b1dda277
yashsinghal-12 <yash.singhal_cs19@gla.ac.in>
2020-10-30 09:44:46 -0700

    create switch() statement to perform the milestones of the race

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -15,6 +15,29 @@ public class Tortoise implements Runnable {
 
     @Override
     public void run() {
+        for (int index = 0; index < MILESTONES; index++) {
+            switch (index) {
+                case 0:
+                    System.out.println("The Tortoise has started from the start line!");
+                    break;
+                case 1:
+                    System.out.println("The Tortoise has reached the Cool River!");
+                    break;
+                case 2:
+                    System.out.println("The Tortoise has reached the Mountain Hill!");
+                    break;
+                case 3:
+                    System.out.println("The Tortoise has reached the Big Oak Tree!");
+                    break;
+                case 4:
+                    System.out.println("The Tortoise has reached the finish line!");
+                    break;
+            }
 
+
+        }
     }
 }
+
+
+
